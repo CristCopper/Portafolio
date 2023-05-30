9e544500b1e8239ca5af8b2e9114ecb78232e48b
Cristian <dhr18cr@gmail.com>
2023-05-29 22:26:35 -0600

    se añadieron elementos

--- index.js
+++ index.js
@@ -15,13 +15,49 @@ function encriptar() {
       document.getElementById("texto").value = textoCifrado;
       tituloMensaje.textContent = "Texto encriptado con éxito";
       parrafo.textContent = "";
-      JakePerro.src = 'img/cara.png';
+      JakePerro.src = './img/console.png';
     } else {
-       JakePerro.src = "img/descarga.png";
+       JakePerro.src = "./img/perro.png";
        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
       swal("Ooops!", "Debes ingresar un texto", "warning");
     }
   }
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let JakePerro = document.getElementById("JakePerro");
   
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        JakePerro.src = "./img/Finn.png";
+      } else {
+        JakePerro.src = "./img/finn1.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
+
+  function copiar(){
+    let texto = document.getElementById("texto").value();
+    navigator.clipboard.writeText(texto.innerHTML);
+    console.log("estoycopiando")
+    // texto.setAttribute();
+    // document.body.appendChild("texto");
+    // texto.select();
+    // document.execCommand('copy');
+    // document.body.removeChild("texto")
+
+  }
   
